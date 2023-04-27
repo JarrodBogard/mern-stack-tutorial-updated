7ffd802dd31f466a877c167206a50ca89fc9236c
Jarrod Bogard <jarrodbogard@gmail.com>
2023-04-27 16:15:21 -0500

    add frontend ui

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -8,10 +8,12 @@ const {
   deleteGoal,
 } = require("../controllers/goalController");
 
-router.route("/").get(getGoals).post(setGoal);
+const { protect } = require("../middleware/authMiddleware");
+
+router.route("/").get(protect, getGoals).post(protect, setGoal);
 // router.get("/", getGoals); router.post("/", setGoal);
 
-router.route("/:id").put(updateGoal).delete(deleteGoal);
+router.route("/:id").put(protect, updateGoal).delete(protect, deleteGoal);
 // router.put("/:id", updateGoal); router.delete("/:id", deleteGoal);
 
 module.exports = router;
---
+++ frontend/src/pages/Register.jsx
@@ -0,0 +1,91 @@
+import { useState, useEffect } from "react";
+import { FaUser } from "react-icons/fa";
+
+function Register() {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    confPassword: "",
+  });
+
+  const { name, email, password, confPassword } = formData;
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <>
+      <section className="heading">
+        <h1>
+          <FaUser /> Register
+        </h1>
+        <p>Please create an account</p>
+      </section>
+
+      <section className="form">
+        <form onSubmit={onSubmit}>
+          <div className="form-group">
+            <input
+              className="form-control"
+              type="text"
+              id="name"
+              name="name"
+              value={name}
+              placeholder="Enter name"
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              className="form-control"
+              type="email"
+              id="email"
+              name="email"
+              value={email}
+              placeholder="Enter email"
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              className="form-control"
+              type="password"
+              id="password"
+              name="password"
+              value={password}
+              placeholder="Enter password"
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              className="form-control"
+              type="password"
+              id="confPassword"
+              name="confPassword"
+              value={confPassword}
+              placeholder="Confirm password"
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <button type="submit" className="btn btn-block">
+              Submit
+            </button>
+          </div>
+        </form>
+      </section>
+    </>
+  );
+}
+
+export default Register;
