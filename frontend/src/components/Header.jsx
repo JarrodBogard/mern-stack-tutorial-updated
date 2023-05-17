3144e98b970a7eec606ebd0026f7edd9d66b623a
Jarrod Bogard <jarrodbogard@gmail.com>
2023-05-17 14:06:48 -0500

    add delete functionality and react build file for production mode

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -17,11 +17,7 @@ function Header() {
   return (
     <header className="header">
       <div className="logo">
-        {user ? (
-          <Link to="/">GoalSetter</Link>
-        ) : (
-          <Link to="/login">GoalSetter</Link>
-        )}
+        <Link to="/">GoalSetter</Link>
       </div>
       <ul>
         {user ? (
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,19 +1,45 @@
 import { useEffect } from "react";
-import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
 
 import GoalForm from "../components/GoalForm";
+import GoalItem from "../components/GoalItem";
+import Spinner from "../components/Spinner";
+import { getGoals, reset } from "../features/goals/goalSlice";
 
 function Dashboard() {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+
   const { user } = useSelector((state) => state.auth);
+  const { goals, isLoading, isError, message } = useSelector(
+    (state) => state.goals
+  );
 
   useEffect(() => {
+    if (isError) {
+      console.log(message);
+    }
+
     if (!user) {
       navigate("/login");
+      // return () => {
+      //   dispatch(reset());
+      // };
     }
-  }, [user, navigate]);
 
+    if (user) {
+      dispatch(getGoals());
+    }
+
+    return () => {
+      dispatch(reset());
+    };
+  }, [user, navigate, isError, message, dispatch]);
+
+  if (isLoading) {
+    return <Spinner />;
+  }
   return (
     <>
       <section className="heading">
@@ -22,8 +48,40 @@ function Dashboard() {
       </section>
 
       <GoalForm />
+
+      <section className="content">
+        {goals.length > 0 ? (
+          <div className="goals">
+            {goals.map((goal) => (
+              <GoalItem key={goal._id} goal={goal} />
+            ))}
+          </div>
+        ) : (
+          <h3>Create A Goal</h3>
+        )}
+      </section>
     </>
   );
 }
 
 export default Dashboard;
+
+// the second useEffect isn't needed. the reason the one useEffect doesn't work is because of the dispatch(getGoals()) being executed causing the payload to get rejected and isError being set to true which causes constant re-renders. Instead check if the user is actually logged in before calling getGoals()
+
+// This will work:
+// useEffect(() => {
+// if(!user){
+//   navigate("/login")
+// }
+// else{
+//  dispatch(getGoals())
+// }
+
+// if(isError){
+// console.log("Error")
+// }
+
+// return () => {
+// dispatch(reset())
+// }
+// }, [user, message, isError, dispatch, navigate])
